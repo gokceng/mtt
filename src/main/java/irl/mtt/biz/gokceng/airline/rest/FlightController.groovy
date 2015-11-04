f3e0b6ebc80f69af607d9cd4161ba4fc2a80ea7d
gokceng <gokcenguner@gmail.com>
2015-11-04 05:17:29 +0300

    rename

--- src/main/java/irl/mtt/biz/gokceng/airline/rest/FlightController.groovy
+++ src/main/java/irl/mtt/biz/gokceng/airline/rest/FlightController.groovy
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController
  */
 @RestController
 @RequestMapping(value = "/flights")
-class FlightControllerService {
+class FlightController {
 	@Autowired
 	private FlightControllerRestClient flightControllerRestClient
 
